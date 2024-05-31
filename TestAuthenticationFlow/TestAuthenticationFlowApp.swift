6250711827faacfd88f401fc8a19f04feb77475b
Nikita <volodko.nikita.1979@gmail.com>
2024-05-31 15:58:01 +0300

    Initial Commit

---
+++ TestAuthenticationFlow/TestAuthenticationFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAuthenticationFlowApp.swift
+//  TestAuthenticationFlow
+//
+//  Created by admin on 31.05.24.
+//
+
+import SwiftUI
+
+@main
+struct TestAuthenticationFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
